d34132ad1970d798249ca3964f997082c7ec3b23
santtuss <142880761+santtuss@users.noreply.github.com>
2024-03-26 13:22:03 +0200

    added w2

---
+++ L2week2/2.py
@@ -0,0 +1,26 @@
+from filefifo import Filefifo
+import time
+
+data = Filefifo(10, name="capture_250Hz_01.txt")
+seconds = 0
+plotting_list = []
+plotting_done = False
+
+def plotting():
+    plotting_list.append(latestvalue)
+
+while True:
+    latestvalue = data.get()
+    seconds += 4
+    
+    if seconds / 100 < 2:
+        plotting()
+    elif not plotting_done:
+        set_max = max(plotting_list)
+        set_min = min(plotting_list)
+        print(set_max, set_min)
+        plotting_done = True
+    if plotting_done and seconds / 100 < 10:
+        scaled = ((latestvalue - set_min) * 100) / (set_max)
+        print(f"scaled {scaled}")
+    time.sleep(0.1)
